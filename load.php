7b196661075811cfc3a4ceefb3653089a5709b4d
kar-iuki <ikariukiirungu@gmail.com>
2024-09-04 14:56:46 +0300

    4th of Septemter

--- load.php
+++ load.php
@@ -1,4 +1,23 @@
 <?php
-require_once "class.php";
-$Obj = new fnc();
-?>
+
+require_once 'class.php';
+
+
+$user = new fnc();
+
+print  $user->user_age("name", "1974 ");
+
+$arr = ["black", "white", "green", "red"];
+
+foreach ($arr as $color) {
+    // print $color . "<br>";
+    print $_SERVER["PHP_SELF"];
+}
+
+print dirname(_FILE_);
+if(file_exists("index.php")){
+    print "yes";
+}
+else{
+    print "no";
+}
